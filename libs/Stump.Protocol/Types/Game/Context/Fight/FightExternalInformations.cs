657946cfa883f3de4ca88f3571444e95c641e8c6
Aerafal13 <aerafal.grimhart@gmail.com>
2022-07-13 17:38:45 +0200

    feat(protocol): add all files of Stump.DofusProtocol

---
+++ libs/Stump.Protocol/Types/Game/Context/Fight/FightExternalInformations.cs
@@ -0,0 +1,50 @@
+﻿namespace Stump.DofusProtocol.Types
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using Stump.DofusProtocol.Types;
+    using System.Collections.Generic;
+    using Stump.Core.IO;
+
+    [Serializable]
+    public class FightExternalInformations
+    {
+        public const short Id  = 117;
+        public virtual short TypeId
+        {
+            get { return Id; }
+        }
+        public ushort FightId { get; set; }
+        public sbyte FightType { get; set; }
+        public int FightStart { get; set; }
+        public bool FightSpectatorLocked { get; set; }
+
+        public FightExternalInformations(ushort fightId, sbyte fightType, int fightStart, bool fightSpectatorLocked)
+        {
+            this.FightId = fightId;
+            this.FightType = fightType;
+            this.FightStart = fightStart;
+            this.FightSpectatorLocked = fightSpectatorLocked;
+        }
+
+        public FightExternalInformations() { }
+
+        public virtual void Serialize(IDataWriter writer)
+        {
+            writer.WriteVarUShort(FightId);
+            writer.WriteSByte(FightType);
+            writer.WriteInt(FightStart);
+            writer.WriteBoolean(FightSpectatorLocked);
+        }
+
+        public virtual void Deserialize(IDataReader reader)
+        {
+            FightId = reader.ReadVarUShort();
+            FightType = reader.ReadSByte();
+            FightStart = reader.ReadInt();
+            FightSpectatorLocked = reader.ReadBoolean();
+        }
+
+    }
+}
