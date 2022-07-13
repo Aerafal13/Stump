2e0912bfcbd966826579bdde52d5ca88f236889a
Aerafal13 <aerafal.grimhart@gmail.com>
2022-07-13 17:52:24 +0200

    feat(base): add all files of Stump.Servers.BaseServer

---
+++ src/Stump.Servers.BaseServer/Exceptions/ExceptionManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using SharpRaven.Data;
+using Stump.Core.Reflection;
+
+namespace Stump.Server.BaseServer.Exceptions
+{
+	public class ExceptionManager : Singleton<ExceptionManager>
+	{
+		private readonly List<Exception> m_exceptions = new List<Exception>();
+
+		public ReadOnlyCollection<Exception> Exceptions => m_exceptions.AsReadOnly();
+
+		public static void RegisterException(Exception ex)
+		{
+			if (ServerBase.IsExceptionLoggerEnabled)
+			{
+				ServerBase.InstanceAsBase.ExceptionLogger.Capture(new SentryEvent(ex));
+			}
+			//m_exceptions.Add(ex);
+		}
+	}
+}
