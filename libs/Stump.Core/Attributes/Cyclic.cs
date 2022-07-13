f275e86b7ad38b0027b1ed64addfd7aabf8ba40b
Aerafal13 <aerafal.grimhart@gmail.com>
2022-07-13 15:39:17 +0200

    feat(core): add all files of Stump.Core

---
+++ libs/Stump.Core/Attributes/Cyclic.cs
@@ -0,0 +1,17 @@
+﻿namespace Stump.Core.Attributes
+{
+	[AttributeUsage(AttributeTargets.Method)]
+	public class Cyclic : Attribute
+	{
+		public Cyclic(int time)
+		{
+			Time = time;
+		}
+
+		public int Time
+		{
+			get;
+			set;
+		}
+	}
+}
